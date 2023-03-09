070624bc2f2f310106ceb65b171a086f2eab580a
Sok Vireak <sokvireak18@kit.edu.kh>
2023-03-09 17:57:49 +0700

    add 404 page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import './NotFound.css'
+import Lottie from 'react-lottie';
+import animationData from '../../assets/lotties/404-error.json'
 
 const NotFound = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
+  const goHome = () => {
+    window.location.href='/'
+  }
+
   return (
-    <div>NotFound</div>
+    <div className='error-container'>
+      <Lottie 
+        style={{width:'80%', maxWidth:'700px', height:'auto'}}
+        options={defaultOptions}
+      />
+      <div className='go-to-homepage-button' onClick={() => goHome()}>Back to Homepage</div>
+  </div>
   )
 }
 
