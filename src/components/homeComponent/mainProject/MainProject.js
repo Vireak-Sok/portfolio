526987672cc50ffd961be4b0423be7ca132471ca
Sok Vireak <sokvireak18@kit.edu.kh>
2023-05-09 11:38:41 +0700

    add thumbnail img

--- src/components/homeComponent/mainProject/MainProject.js
+++ src/components/homeComponent/mainProject/MainProject.js
@@ -3,17 +3,21 @@ import './MainProject.css'
 import MainProjectCard from '../../card/mainProjectCard/MainProjectCard'
 
 const MainProject = () => {
+  const project1 = require('../../../assets/projectThumbnail/mobileAppProject.png')
+  const project2 = require('../../../assets/projectThumbnail/webProject.png')
+  const project3 = require('../../../assets/projectThumbnail/tabletProject.png')
+  const project4 = require('../../../assets/projectThumbnail/watchProject.png')
   return (
     <>
     <div className='seperator' style={{height: '18rem', width: '100%', backgroundImage: 'linear-gradient(var(--mainBlue), var(--linkBlue))'}}/>
     <div className='main-project-container'>
       <div className='row-container'>
-        <MainProjectCard/>
-        <MainProjectCard/>
+        <MainProjectCard thumbnail={project1} bgColor={'#14949A'} shadow={'#006B71 0px 5px 15px'}/>
+        <MainProjectCard thumbnail={project2} bgColor={'#56965E'} shadow={'#376A3D 0px 5px 15px'}/>
       </div>
       <div className='row-container'>
-        <MainProjectCard/>
-        <MainProjectCard/>
+        <MainProjectCard thumbnail={project3} bgColor={'#fff'} shadow={'#ff0 0px 5px 15px'}/>
+        <MainProjectCard thumbnail={project4} bgColor={'#825E9E'} shadow={'#2A0844 0px 5px 15px'}/>
       </div>
     </div>
     </>
