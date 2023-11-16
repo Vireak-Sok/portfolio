9ee20dc7a6f024fbf919d3556b84abab502c8bf7
Sok Vireak <sokvireak18@kit.edu.kh>
2023-11-16 13:55:56 +0700

    Update OutlineButton.jsx

--- src/components/OutlineButton.jsx
+++ src/components/OutlineButton.jsx
@@ -8,9 +8,13 @@ function OutlineButton({label, url, hover, icon, dowload, download_url}) {
     anchor.click();
     anchor.parentNode.removeChild(anchor);
   }
+
+  function redirectFunc(){
+    window.open({url}, "_blank");
+  }
   
   return (
-    <button onclick={dowload ? () => downloadFunc() : null} class={`w-max flex items-center justify-center gap-2 pb-2 text-base text-slate-400 capitalize border-b border-slate-400 font-medium hover:cursor-pointer ${hover} transition-all ease-in-out`} href={url} target="_blank">
+    <button onclick={dowload ? () => downloadFunc() : () => redirectFunc()} class={`w-max flex items-center justify-center gap-2 pb-2 text-base text-slate-400 capitalize border-b border-slate-400 font-medium hover:cursor-pointer ${hover} transition-all ease-in-out`} href={url} target="_blank">
       {label} {icon}
     </button>
   )
